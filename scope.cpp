a74cd2c1458f2c35e51393061cccb4e3d1a54754
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2017-10-18 12:26:34 +0530

    program to test scope of variables

---
+++ scope.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+int g=65;
+int main()
+{	    int a=12;
+		int b=13;
+		int g=14;
+		int c=15;
+	{
+		int a=44;
+		int b=36;
+		int g=87;
+		int c=56;
+		cout<<" inside bracket\n";
+		cout<<"a:"<<a<<" ";
+	    cout<<"b:"<<b<<" ";
+	    cout<<"g:"<<g<<" ";
+	    cout<<"c:"<<c<<" ";
+
+	}
+	cout<<" outside bracket";
+	cout<<"a:"<<a<<" ";
+	cout<<"b:"<<b<<" ";
+	cout<<"g:"<<g<<" ";
+	cout<<"c:"<<c<<" ";
+	cout<<"global g:";
+	cout<<::g;// indicates error if g is not  defined globally
+	return 0;
+}
+
