766e92daaceaee46824abd5d6575d74edc8f0824
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2017-11-06 15:39:27 +0530

    Largest number with given sum
    A boy lost the password of his super locker. He remembers the number of digits N as well as the sum S of all the digits of his password. He know that his password is the largest number of N digits that can be possible with given sum S. As he is busy doing his homework, help him retrieving his password.

---
+++ largestNo.cpp
@@ -0,0 +1,60 @@
+//Largest number with given sum
+using namespace std;
+#include <iostream>
+int power(int base,int exponent)
+{
+	int ans=1;
+	for(int i=0;i<exponent;i++)
+	{
+		ans=ans*base;
+	}
+	return ans;
+}
+
+int main()
+{
+	int number;
+	int add;
+	cout<<" enter number of digits : ";
+	cin>> number;
+	cout<<" enter sum of digits: ";
+	cin>>add;
+	int temp;
+	int result=0;
+	 if(add>number*9)
+	 {
+	 	cout<<"no such number  exist:";
+	 
+	 }
+	 else
+	 {
+
+
+	while(number>0)
+	{
+		temp=power(10,number-1);
+		if(add>=9)
+		{
+			//cout<<" temp is: "<< temp;
+			temp=temp*9;
+			//cout<<"multiplying: "<<temp;
+			add=add-9;
+			result=result+temp;
+			//cout<<" rsult: "<<result;
+		}
+		else if(add<9)
+		{
+			//cout<<" temp is: "<< temp;
+			temp=temp*add;
+			//cout<<"multiplying: "<<temp;
+			result=result+temp;
+			//cout<<" rsult: "<<result;
+			break;
+		}
+		number=number-1;
+	}// end while
+	cout<<endl;
+	cout<<" largest number with given sum of digits is: "<<result;
+  }// end else
+ return 0;
+}
