7fadd152dce0e17cde6a9999ee13eacab4ff33bf
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2017-11-28 12:50:37 +0530

    Delete forloop1.cpp

--- interview_bit/forloop1.cpp
+++
@@ -1,8 +0,0 @@
- int i, j, k = 0;
-    for (i  = n/2; i <= n; i++) {
-        for (j = 2; j <= n; j = j * 2) {
-            k = k + n/2;
-        }
-    }
-    
-    /* time complexity: theta(nlogn)  */
