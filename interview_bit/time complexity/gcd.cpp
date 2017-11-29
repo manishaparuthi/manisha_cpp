f78a72df17f12a077cc7bad5766bda24ec7bbd91
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2017-11-29 18:25:08 +0530

    Create gcd.cpp

---
+++ interview_bit/time complexity/gcd.cpp
@@ -0,0 +1,15 @@
+ int gcd(int n, int m) {
+            if (n%m ==0) return m;
+            if (n < m) swap(n, m);
+            while (m > 0) {
+                n = n%m;
+                swap(n, m);
+            }
+            return n;
+    }
+
+
+   /*
+   
+   logn(base m)
+*/
