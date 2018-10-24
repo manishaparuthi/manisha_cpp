65ad29ac03230fa18aa94e461adf1728673109e9
manishaparuthi <31645396+manishaparuthi@users.noreply.github.com>
2018-10-24 10:33:02 +0530

    Update Q-1.cpp

--- Algo_Assignment/Section A/Q-1.cpp
+++ Algo_Assignment/Section A/Q-1.cpp
@@ -1,6 +1,5 @@
 /*
 chain length is 2 we'll fill m[1,2] m[2,3] m[3,4]  and so on.
-
 for chain length 'l' and first index i , j will be i+l-1 and k will vary from i to j.
 */
 
@@ -24,7 +23,7 @@ int matrix_chain(vector<int> dimension)
 	 
 	for (int i=1; i<size; i++) //chain length = 1 means no. of multiplications =0
 		m[i][i] = 0; 
-
+	int count=0;
 	// L is chain length. or no of matrices
 	for (int L=2; L<size; L++) 
 	{ 
@@ -34,13 +33,13 @@ int matrix_chain(vector<int> dimension)
 			m[i][j] = INT_MAX; 
 			for (int k=i; k<=j-1; k++) 
 			{ 
-				
-				m[i][j] = min ( m[i][j] , m[i][k] + m[k+1][j] + dimension[i-1]*dimension[k]*dimension[j]); 
+				count++;
+				m[i][j] = min ( m[i][j] , m[i][k] + m[k+1][j] + dimension[i-1]*dimension[k]*dimension[j] ); 
 				
 			} 
 		} 
 	} 
-
+	cout<<"Total no of comparisons: "<<count<<"\n";
     return m[1][size-1]; 
 } 
 
@@ -48,15 +47,34 @@ int matrix_chain(vector<int> dimension)
 int main() 
 { 
 	cout<<"Enter no. of matrices: ";
-	int n,x;
+	int n,prevrow,prevcolumn,row,column;
 	vector<int> dimension;
 	cin>>n;
-	cout<<"Enter dimensions:";
-	for(int i=0;i<=n;i++)
-	{   cin>>x;
-	    dimension.push_back(x);
-	    //cin>>dimension[i];
-	}
-	cout<<"Minimum no. of multiplications will be: "<<matrix_chain(dimension);
+	
+	bool scontinue=false;
+	do
+	{
+	    scontinue=false;
+	    cout<<"Enter dimensions of matrix "<<1<<": ";
+	    cin>> prevrow>>prevcolumn;
+	     dimension.push_back(prevrow);
+	for(int i=1;i<n;i++)
+	    {   
+		cout<<"Enter dimensions of matrix "<<i+1<<": ";
+		cin>> row>> column;
+		if(row!=prevcolumn)
+		{
+			cout<<"error";
+			cout<<"Enter again"<<"\n";
+			scontinue=true;
+			break;	
+		}
+		prevrow=row;
+		prevcolumn=column;
+		dimension.push_back(row);
+		
+	   }
+	}while(scontinue);
+	cout<<"Minimum no. of multiplications will be: "<<matrix_chain(dimension)<<"\n";
 	return 0; 
 } 
