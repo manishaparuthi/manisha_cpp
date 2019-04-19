401fd3882ca0effe0abc8befaf1ebd259b39c7d5
Manisha Paruthi <31645396+manishaparuthi@users.noreply.github.com>
2019-04-19 20:20:49 +0530

    Create repeatAndMissing.cpp

---
+++ interview_bit/arrays/repeatAndMissing.cpp
@@ -0,0 +1,56 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A) {
+    int X = 0;
+   
+    
+    int n = A.size();
+    for( int i = 0 ; i < n ; i++)
+    {
+        X^=A[i];
+        
+    }
+    for( int i = 1 ; i <= n ; i++)
+    {
+        X^=i;
+        
+    }
+    int set_bit_no = X & -X; //choose one set bit ,here we are choosing rightmost set bit
+  
+    int x = 0, y = 0; // two numbers 
+    for (int i = 0; i <= n-1; i++) 
+    { 
+        if (A[i] & set_bit_no) 
+            x = x ^ A[i]; 
+        else
+            y = y ^ A[i]; 
+    } 
+    for (int i = 1; i <= n; i++) 
+    { 
+        if (i & set_bit_no) 
+            x = x ^ i; /* XOR of first set in arr[] and 
+                         {1, 2, ...n }*/
+        else
+            y = y ^ i; /* XOR of second set in arr[] and 
+                         {1, 2, ...n } */
+    } 
+    
+  int flagx=0;
+  for(int i = 0 ; i < n ; i ++)
+  {
+      if(x==A[i]){
+          flagx=1;
+          break;
+      }
+  }
+   vector<int>result;
+  
+  if(flagx){
+      result.push_back(x);
+      result.push_back(y);
+  }
+  
+  else{
+    result.push_back(y);
+    result.push_back(x);
+  }
+    return result;
+}
